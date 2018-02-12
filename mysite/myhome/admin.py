4887fe56a4d364f47e222ea6067beb8ba2a398c0
rupesh-singh <rupesh08singh08@gmail.com>
2018-02-12 17:12:51 +0530

    database created

--- mysite/myhome/admin.py
+++ mysite/myhome/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import intent, answers, questions
+
+admin.site.register(intent)
+admin.site.register(answers)
+admin.site.register(questions)
 
 # Register your models here.
--- mysite/myhome/models.py
+++ mysite/myhome/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class intent(models.Model):
+	intent_name=models.CharField(max_length=250)
+
+
+class answers(models.Model):
+	intent_id=models.ForeignKey(intent, on_delete=models.CASCADE)
+	answer=models.CharField(max_length=2000)
+	score=models.CharField(max_length=50,default="GOOD")
+
+class questions(models.Model):
+	intent_id=models.ForeignKey(intent, on_delete=models.CASCADE)
+	questions=models.CharField(max_length=1000)
+
+
+
+
+
+
+
