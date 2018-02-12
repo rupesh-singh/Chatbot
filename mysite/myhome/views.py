82b467bef5b81be6c3afe903eb00a9e04721e912
rupesh-singh <rupesh08singh08@gmail.com>
2018-02-12 22:23:37 +0530

    json file loaded

--- mysite/myhome/views.py
+++ mysite/myhome/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+from .models import intent, answers, questions
 
 import subprocess
 
@@ -15,4 +16,29 @@ def index(request):
 	#print(newst)
 	xa = subprocess.check_output(['python3', '/home/rupesh/Desktop/Web-django/mysite/myhome/response1.py', newst])
 	#print(xa)
+	#fill();
 	return HttpResponse(xa[20:])
+
+'''
+import json
+
+
+def fill():
+	with open('/home/rupesh/Desktop/project/mysite/myhome/int.json') as data_file:
+		data = json.load(data_file)
+	for  i in data['intents']:
+		#a=intent(intent_name=i['tag'])
+		#a.save();
+		for j in i['patterns']:
+			p = intent.objects.get(intent_name=i['tag'])
+			a=questions(intent_id=p,questions=j)
+			a.save()
+			
+		for j in i['responses']:
+			p = intent.objects.get(intent_name=i['tag'])
+			a=answers(intent_id=p,answer=j)
+			a.save()
+
+
+
+'''
